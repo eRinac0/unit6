14196af236e43494ccc599d881a421a6c840ab24
Vladik <vladkramarenko53@gmail.com>
2023-04-19 19:48:14 +0300

    task8

---
+++ js/script8.js
@@ -0,0 +1,17 @@
+let Manager={
+    first_name:undefined,
+    second_name:undefined,
+    age:undefined,
+    fillobject: function(first_name,second_name,age){
+        this.first_name=first_name;
+        this.second_name=second_name;
+        this.age=age;
+    }  ,
+    output: function(){
+        console.log(this.first_name);
+        console.log(this.second_name);
+        console.log(this.age);
+    }
+}
+Manager.fillobject("Vlad","Kramarenko","18");
+Manager.output();
